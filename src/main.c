302a6cb71e497504f0163bc0eb2a53ee01a0a206
teakhanirons <62670141+teakhanirons@users.noreply.github.com>
2020-05-17 20:53:18 +0300

    24 bit RGB BMP mod

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /*
-  Custom Boot Splash
+  Custom Boot Splash BMP
   Copyright (C) 2018, Princess of Sleeping
+  Copyright (C) 2020, teakhanirons
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -13,6 +14,8 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+// ur0:tai/boot_splash.bmp needs to be 24 bit RGB
+
 #include <psp2kern/kernel/modulemgr.h>
 #include <psp2kern/kernel/sysmem.h>
 #include <psp2kern/kernel/threadmgr.h>
@@ -20,6 +23,7 @@
 #include <psp2kern/io/fcntl.h>
 #include <psp2kern/io/stat.h>
 #include <psp2kern/display.h>
+#include <psp2kern/kernel/dmac.h> 
 
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args){
@@ -29,28 +33,55 @@ int module_start(SceSize argc, const void *args){
 	SceIoStat stat;
 
 	void *fb_addr = NULL;
+	void *bmp_addr = NULL;
 
-	int stat_ret, uid, fd;
+	int stat_ret, uid, rgbuid, fd;
 
-	stat_ret = ksceIoGetstat("ur0:tai/boot_splash.bin", &stat);
+	stat_ret = ksceIoGetstat("ur0:tai/boot_splash.bmp", &stat);
 
-	if((stat_ret < 0) || ((uint32_t)stat.st_size != 0x1FE000) || (SCE_SO_ISDIR(stat.st_mode) != 0)){
-		return SCE_KERNEL_START_SUCCESS;
-	}
+	// 24 bit RGB BMP + 54 byte header = 0x17E836 size
+	if((stat_ret < 0) || ((uint32_t)stat.st_size != 0x17E836) || (SCE_SO_ISDIR(stat.st_mode) != 0)) return SCE_KERNEL_START_SUCCESS;
+
+	ksceDebugPrintf("size okke!\n");
 
 	optp.size = 0x58;
 	optp.attr = 2;
 	optp.paddr = 0x1C000000;
 
-	if((uid = ksceKernelAllocMemBlock("SceDisplay", 0x6020D006, 0x200000, &optp)) < 0){
-		return SCE_KERNEL_START_SUCCESS;
-	}
+	if((uid = ksceKernelAllocMemBlock("SceDisplay", 0x6020D006, 0x200000, &optp)) < 0) return SCE_KERNEL_START_SUCCESS;
+
+	ksceDebugPrintf("disple okke!\n");
+
+	if((rgbuid = ksceKernelAllocMemBlock("rgb", 0x1050D006, 0x200000, NULL)) < 0) return SCE_KERNEL_START_SUCCESS;
+
+	ksceDebugPrintf("rgb okke!\n");
 
 	ksceKernelGetMemBlockBase(uid, (void**)&fb_addr);
+	ksceKernelGetMemBlockBase(rgbuid, (void**)&bmp_addr);
 
-	fd = ksceIoOpen("ur0:tai/boot_splash.bin", SCE_O_RDONLY, 0);
-	ksceIoRead(fd, fb_addr, 0x1FE000);
+	ksceDmacMemset(fb_addr, 0xFF, 0x1FE000); // alpha will be FF, this is fallback
+	ksceDebugPrintf("memset okke!\n");
+
+	fd = ksceIoOpen("ur0:tai/boot_splash.bmp", SCE_O_RDONLY, 0);
+	ksceIoLseek(fd, 54,	SCE_SEEK_SET); // 54 byte bmp header
+	ksceIoRead(fd, bmp_addr, 0x17E7E2); // 0x17E836 - 54 = 0x17E7E2
 	ksceIoClose(fd);
+    ksceDebugPrintf("bmp read okke!\n");
+
+    for(int i = 544 - 1; i >= 0; i--) {
+    	for(int j = 0; j < 960; j++) {
+    		// A B G R
+        	*(uint32_t *)(fb_addr + ((i * 960) + j) * 4) =
+            	((((char *)bmp_addr)[((i * 960) + j) * 3 + 2]) <<  0) |
+    	        ((((char *)bmp_addr)[((i * 960) + j) * 3 + 1]) <<  8) |
+        	    ((((char *)bmp_addr)[((i * 960) + j) * 3 + 0]) << 16) |
+            	0xFF000000;
+		}
+    }
+
+	ksceDebugPrintf("display okke!\n");
+
+	ksceKernelFreeMemBlock(rgbuid);
 
 	ksceKernelCpuDcacheAndL2WritebackInvalidateRange(fb_addr, 0x1FE000);
 
